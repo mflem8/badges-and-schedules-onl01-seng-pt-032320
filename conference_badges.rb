1c6a959eeacb4b23e0b128b3099a73e9bea6727d
Matt Fleming <matthewfleming8@gmail.com>
2020-03-31 02:49:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,9 +15,9 @@ room_numbers = [1, 2, 3, 4, 5, 6, 7]
 
 def assign_rooms(attendees)
   room_assignments = []
-  attendees.each_with_index do |name, room| room_assignments << "Hello, #{name}! You'll be assigned to room #{room}!"
+  attendees.each_with_index do |name, room| room_assignments << "Hello, #{name}! You'll be assigned to room #{room+1}!"
  end
- room_assignments[1..7]
+ room_assignments
  end
  
   
